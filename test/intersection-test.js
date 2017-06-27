441bc2286d7a5d35816bb62cd27a8f73aa35e4f6
Maxime Soulard <maxime.soulard-prestataire@labanquepostale.fr>
2017-06-27 09:23:47 +0200

    Unit test : Get closest from Delta no solution

--- test/intersection-test.js
+++ test/intersection-test.js
@@ -5,32 +5,46 @@ QUnit.module( "Intersection", {});
 
 QUnit.test("Rayon doesn't intersect sphere", function(assert) {
     assert.expect(1);
-
+    // ARRANGE
     const sphere1 = {rayon: 70, centre: new Vector(150.0, 150.0, -100)};
     var rayon = {origin: new Vector(0, 0, 1000), direction: new Vector(0, 0, -1)};
     const intersection = new Intersection(rayon, sphere1);
+    // ACT
     const resultToAssert = intersection.getClosestIntersect();
-    
+    // ASSERT
     assert.strictEqual(resultToAssert, undefined);
 });
 
 QUnit.test("Calculate Delta", function(assert) {
     assert.expect(1);
-
+    // ARRANGE
     const sphere1 = {rayon: 70, centre: new Vector(150.0, 150.0, -100)};
     var rayon = {origin: new Vector(0, 0, 1000), direction: new Vector(0, 0, -1)};
     const intersection = new Intersection(rayon, sphere1);
-
+    // ACT
     let result = intersection.calculateDelta(5, 4, 8);
-
+    // ASSERT
     assert.strictEqual(result, -144); // 4^2 - 4 * 5 * 8 = -144;
+});
+
+QUnit.test("Get closest from Delta - no solution", function(assert) {
+    assert.expect(1);
+    // ARRANGE
+    const sphere1 = {rayon: 70, centre: new Vector(150.0, 150.0, -100)};
+    var rayon = {origin: new Vector(0, 0, 1000), direction: new Vector(0, 0, -1)};
+    const intersection = new Intersection(rayon, sphere1);
+    // ACT
+    let result = intersection.getClosestFromDelta(5, 4, 8);
+    // ASSERT
+    assert.strictEqual(result, undefined); // 4^2 - 4 * 5 * 8 = -144; so delta is undefined
 });
 
 QUnit.test("Random ray intersects sphere", function(assert) {
     assert.expect(1);
-
+    // ARRANGE
     const sphere1 = {rayon: 70, centre: new Vector(150.0, 150.0, -100)};
     let isIntersected = false;
+    // ACT
     while (!isIntersected) {
         const oX = Math.round(Math.random() * (300 - 0) + 0);
         const oY = Math.round(Math.random() * (300 - 0) + 0);
@@ -44,6 +58,6 @@ QUnit.test("Random ray intersects sphere", function(assert) {
         const resultToAssert = intersection.getClosestIntersect();
         isIntersected = typeof resultToAssert != "undefined";
     }
-
+    // ASSERT
     assert.ok(isIntersected);
 });
--- test/vector-test.js
+++ test/vector-test.js
@@ -3,45 +3,51 @@ const Vector = require("../lib/vector.js");
 QUnit.module( "Vector", {});
 
 QUnit.test("Add two vectors, returns a new vector", function(assert) {
+    // ARRANGE
     const vec1 = new Vector(10, 10, 10), vec2 = new Vector(10, 10, 10);
     const expected = {x: 20, y: 20, z: 20};
     // ACT
     const result = vec1.add(vec2);    
-
+    // ASSERT
     assert.deepEqual(returnCoordinatesObject(result), expected);
 });
 
 QUnit.test("Sub two vectors, returns a new vector", function(assert) {
+    // ARRANGE
     const vec1 = new Vector(10, 10, 10), vec2 = new Vector(10, 10, 10);
     const expected = {x: 0, y: 0, z: 0};
     // ACT
     const result = vec1.sub(vec2);
-
+    // ASSERT
     assert.deepEqual(returnCoordinatesObject(result), expected);
 });
 
 QUnit.test("Dot two vectors, returns a new vector", function(assert) {
+    // ARRANGE
     const vec1 = new Vector(10, 10, 10), vec2 = new Vector(10, 10, 10);
     const expected = {x: 100, y: 100, z: 100};
     // ACT
     const result = vec1.dot(vec2);
-
+    // ASSERT
     assert.deepEqual(returnCoordinatesObject(result), expected);
 });
 
 QUnit.test("Cross product two vectors, returns a new vector", function(assert) {
+    // ARRANGE
     const vec1 = new Vector(10, 15, 25), vec2 = new Vector(20, 25, 35);
     const expected = {x: -100, y: 150, z: -50};
     // ACT
     const result = vec1.crossProduct(vec2);
-
+    // ASSERT
     assert.deepEqual(returnCoordinatesObject(result), expected);
 });
 
 QUnit.test("Type error is thrown", function(assert) {
+    // ARRANGE
     const vec1 = new Vector(10, 15, 25), vec2 = {};
     const expected = {x: -100, y: 150, z: -50};
     // ACT
+    // ASSERT
     assert.throws(function() { 
             vec1.crossProduct(vec2);
         }, TypeError, "A TypeError is thrown");
