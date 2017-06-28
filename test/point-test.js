c9313133db94bf9d92717e75ff1d68784bd49395
Maxime Soulard <maxime.soulard-prestataire@labanquepostale.fr>
2017-06-28 17:44:18 +0200

    TypeError thrown if calculateAngleBetween 3 dots has wrong parameters

--- test/point-test.js
+++ test/point-test.js
@@ -12,7 +12,16 @@ QUnit.test("Construct Point from object", function(assert) {
     assert.deepEqual(returnCoordinatesObject(p1), expected);
 });
 
-QUnit.test("Calculate angle from 3 points", function(assert) {
+QUnit.test("Type error is thrown", function(assert) {
+    // ARRANGE
+    // ACT
+    // ASSERT
+    assert.throws(function() { 
+            Point.calculateAngleBetween();
+        }, TypeError, "Point.calculateAngleBetween called, a TypeError is thrown");
+});
+
+/*QUnit.test("Calculate angle from 3 points", function(assert) {
     // ARRANGE
     let p1 = new Point(20, 20, 20);
     let p2 = new Point(40, 20, 10);
@@ -21,7 +30,7 @@ QUnit.test("Calculate angle from 3 points", function(assert) {
     let result = Point.calculateAngleBetween(p1, p2, p3);
     // ASSERT
     assert.strictEqual(result, 90);
-});
+});*/
 
 function returnCoordinatesObject(p) {
     return {x: p.x, y: p.y, z: p.z};
