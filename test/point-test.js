54bf0bf6fc11f35706400c2b5625ec6856facbc2
Maxime Soulard <maxime.soulard-prestataire@labanquepostale.fr>
2017-06-28 19:17:06 +0200

    Point : length between 2 points and Angle between 3 (in rad and deg)

--- test/point-test.js
+++ test/point-test.js
@@ -17,20 +17,31 @@ QUnit.test("Type error is thrown", function(assert) {
     // ACT
     // ASSERT
     assert.throws(function() { 
-            Point.calculateAngleBetween();
-        }, TypeError, "Point.calculateAngleBetween called, a TypeError is thrown");
+            Point.angleBetween();
+        }, TypeError, "Point.angleBetween called, a TypeError is thrown");
 });
 
-/*QUnit.test("Calculate angle from 3 points", function(assert) {
+QUnit.test("Length between two points", function(assert) {
     // ARRANGE
     let p1 = new Point(20, 20, 20);
-    let p2 = new Point(40, 20, 10);
-    let p3 = new Point(10, 40, 20);
+    let p2 = new Point(30, 10, 0);
+    const expected = 24;
     // ACT
-    let result = Point.calculateAngleBetween(p1, p2, p3);
+    let result = Point.lengthBetween(p1, p2);
     // ASSERT
-    assert.strictEqual(result, 90);
-});*/
+    assert.strictEqual(Math.round(result), expected, "length P1P2 = sqrt((20 - 30)² + (20 - 10)² + (20 - 0)²) ~= 24");
+});
+
+QUnit.test("Calculate angle from 3 points", function(assert) {
+    // ARRANGE
+    let p1 = new Point(200, 20, 20);
+    let p2 = new Point(40, 200, 10);
+    let p3 = new Point(10, 40, 200);
+    // ACT
+    let result = Point.angleBetween(p1, p2, p3).toDegrees();
+    // ASSERT
+    assert.strictEqual(Math.round(result), 59);
+});
 
 function returnCoordinatesObject(p) {
     return {x: p.x, y: p.y, z: p.z};
