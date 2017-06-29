2b1057d2bf7389f10d6bdbc938af1b7f5435d8ba
Maxime Soulard <maxime.soulard-prestataire@labanquepostale.fr>
2017-06-29 16:30:54 +0200

    Point.js tests from QUnit to chai.js

--- test/point-test.js
+++ test/point-test.js
@@ -1,48 +1,44 @@
+const chai = require('chai');
+const expect = chai.expect;
 const Point = require("../lib/point.js");
 
-QUnit.module( "Point", {});
-
-QUnit.test("Construct Point from object", function(assert) {
-    // ARRANGE
-    let p1 = new Point();
-    const expected = {x: 20, y: 20, z: 20};
-    // ACT
-    p1 = p1.fromObject({x: 20, y: 20, z: 20});
-    // ASSERT
-    assert.deepEqual(returnCoordinatesObject(p1), expected);
+describe('Point', function() {
+  it('should throw TypeError exception when instanceof param NOT object', typeErrorThrown);
+  it('should return length between P1(20, 20, 20) and P2(30, 10, 0) = sqrt((20 - 30)² + (20 - 10)² + (20 - 0)²) ~= 24', lengthBetween);
+  it('should calculate angle from 3 points', angleFrom3Points);
 });
 
-QUnit.test("Type error is thrown", function(assert) {
+function typeErrorThrown(done) {
     // ARRANGE
-    // ACT
-    // ASSERT
-    assert.throws(function() { 
-            Point.angleBetween();
-        }, TypeError, "Point.angleBetween called, a TypeError is thrown");
-});
+    const angleBetween = function() {
+        Point.angleBetween();
+    };
+    // ACT & ASSERT
+    expect(angleBetween).to.throw(TypeError);
+    done();
+}
 
-QUnit.test("Length between two points", function(assert) {
+function lengthBetween(done) {
     // ARRANGE
-    let p1 = new Point(20, 20, 20);
-    let p2 = new Point(30, 10, 0);
+    const p1 = new Point(20, 20, 20);
+    const p2 = new Point(30, 10, 0);
     const expected = 24;
     // ACT
-    let result = Point.lengthBetween(p1, p2);
+    const result = Point.lengthBetween(p1, p2);
     // ASSERT
-    assert.strictEqual(Math.round(result), expected, "length P1P2 = sqrt((20 - 30)² + (20 - 10)² + (20 - 0)²) ~= 24");
-});
+    expect(expected).to.equal(Math.round(result));
+    done();
+}
 
-QUnit.test("Calculate angle from 3 points", function(assert) {
+function angleFrom3Points(done) {
     // ARRANGE
-    let p1 = new Point(200, 20, 20);
-    let p2 = new Point(40, 200, 10);
-    let p3 = new Point(10, 40, 200);
+    const p1 = new Point(200, 20, 20);
+    const p2 = new Point(40, 200, 10);
+    const p3 = new Point(10, 40, 200);
+    const expected = 59;
     // ACT
-    let result = Point.angleBetween(p1, p2, p3).toDegrees();
+    const result = Point.angleBetween(p1, p2, p3).toDegrees();
     // ASSERT
-    assert.strictEqual(Math.round(result), 59);
-});
-
-function returnCoordinatesObject(p) {
-    return {x: p.x, y: p.y, z: p.z};
+    expect(expected).to.equal(Math.round(result));
+    done();
 }
